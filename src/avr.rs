64254e8154b58995f7ab1a3aae74bfdb0aaf0154
kazukiyoshida <kazukiyoshida@kazuki-macbook.local>
2020-01-28 16:34:49 +0900

    feat: define Word type as a iterator

--- src/avr.rs
+++ src/avr.rs
@@ -1,3 +1,5 @@
+use std::iter::IntoIterator;
+
 pub trait AVR {
     // Program Counter
     fn pc(&self) -> u16;
@@ -19,3 +21,55 @@ pub trait Memory<T> {
     fn set(&mut self, a: usize, v: T);
 }
 
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub struct Word(pub u16);
+
+impl Word {
+    pub fn top(&self) -> u8 {
+        ( self.0 >> 8 ) as u8
+    }
+    pub fn low(&self) -> u8 {
+        ( self.0 & 0b11111111 ) as u8
+    }
+}
+
+pub struct WordIter {
+    seeker: u8,
+    word: u16,
+}
+
+impl IntoIterator for Word {
+    type Item = bool;
+    type IntoIter = WordIter;
+    fn into_iter(self) -> Self::IntoIter {
+        WordIter {
+            seeker: 0,
+            word: self.0,
+        }
+    }
+}
+
+impl Iterator for WordIter {
+    type Item = bool;
+
+    // Seek each bit from right to left.
+    fn next(&mut self) -> Option<bool> {
+        if self.seeker >= 16 {
+            return None
+        }
+        let bit = ( self.word & ( 1 << self.seeker )) >> self.seeker;
+        self.seeker += 1;
+        Some(bit == 1)
+    }
+}
+
+#[test]
+fn test_u8_word() {
+    let w = Word(0b01001100_11001100);
+    for b in w.into_iter() {
+        println!("{}", b);
+    }
+    let w1 = Word(0b00001111_11110000);
+    println!("{:08b}", w1.top());
+    println!("{:08b}", w1.low());
+}
