be64c1cc2f9c66b7b3089be907a1327ae7836132
kazukiyoshida <kazukiyoshida@kazuki-macbook.local>
2020-02-28 16:49:03 +0900

    chore: update flow example file

--- examples/flow.rs
+++ examples/flow.rs
@@ -1,14 +1,11 @@
 use avr_emulator::atmega328p::*;
 use avr_emulator::avr::*;
-use avr_emulator::logger::*;
-use std::collections::VecDeque;
-use std::env;
-use std::{thread, time};
+// use std::{thread, time};
 
 pub const SAMPLE_FILE_NAME: &str = "hex/avr_studio/led_flashing_fast/led_flashing.hex";
 
 fn main() {
-    let ds = time::Duration::from_millis(1);
+    // let ds = time::Duration::from_millis(1);
     let mut s = 0;
 
     let avr = ATmega328P::new();
@@ -27,13 +24,6 @@ fn main() {
     loop {
         s += 1;
         avr.execute();
-        if s % 100 == 0 {
-            println!(
-                "cycle = {:10}    tcnt1 = {:6}   PORTB = {:08b}",
-                avr.cycle(),
-                avr.get_word(timer1.tcnt),
-                portb.avr.get_register(portb.ddrx)
-            );
         timer0.clk_io();
         timer1.clk_io();
         timer2.clk_io();
@@ -41,8 +31,10 @@ fn main() {
         portc.clk_io();
         portd.clk_io();
 
+        if s % 10_000 == 0 {
+            println!("cycle = {:10}    PORTB = {}", avr.cycle(), portb,);
         }
 
-        thread::sleep(ds);
+        // thread::sleep(ds);
     }
 }
